20a7907591a2953fc3814d6f4a9613f8098050ab
Mitch68427 <84968045+Mitch68427@users.noreply.github.com>
2022-12-10 20:30:56 +1000

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,1 +1,13 @@
+<?php
+
+namespace Mitch\Roulette;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+
+class Main extends PluginBase implements Listener {
+  
+}
 
