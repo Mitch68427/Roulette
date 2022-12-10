1c086bb66132fc0ec4dec44d9a1cc3383f0183b1
Mitch68427 <84968045+Mitch68427@users.noreply.github.com>
2022-12-10 20:57:27 +1000

    Update rltCommand.php

--- src/commands/rltCommand.php
+++ src/commands/rltCommand.php
@@ -4,7 +4,7 @@ namespace Mitch68427\Roulette;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class rltCommands extends Pluginbase implements Listener {
+class rltCommands implements Listener {
   var $colour
   var $amount
     
